2add9a76d5c96725ba476dde6d2e41a2bf5d94a0
Shuhrat <shux121995@gmail.com>
2021-05-21 14:32:31 +0500

    draft version of home page completed

--- src/views/Index.js
+++ src/views/Index.js
@@ -150,7 +150,7 @@ const Index = (props) => {
               <CardHeader className="border-0">
                 <Row className="align-items-center">
                   <div className="col">
-                    <h3 className="mb-0">Page visits</h3>
+                    <h3 className="mb-0">Graduates by department</h3>
                   </div>
                   <div className="col text-right">
                     {/*<Button
@@ -167,55 +167,38 @@ const Index = (props) => {
               <Table className="align-items-center table-flush" responsive>
                 <thead className="thead-light">
                   <tr>
-                    <th scope="col">Page name</th>
-                    <th scope="col">Visitors</th>
-                    <th scope="col">Unique users</th>
-                    <th scope="col">Bounce rate</th>
+                    <th scope="col">Deparment title</th>
+                    <th scope="col">Graduates</th>
+                    <th scope="col">Male</th>
+                    <th scope="col">Female</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr>
-                    <th scope="row">/argon/</th>
+                    <th scope="row">Computer Science and Engineering</th>
                     <td>4,569</td>
                     <td>340</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 46,53%
-                    </td>
+                    <td>25</td>
                   </tr>
                   <tr>
-                    <th scope="row">/argon/index.html</th>
+                    <th scope="row">
+                      Information and Communication Engineering
+                    </th>
                     <td>3,985</td>
                     <td>319</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      46,53%
-                    </td>
+                    <td>46</td>
                   </tr>
                   <tr>
-                    <th scope="row">/argon/charts.html</th>
+                    <th scope="row">Business Administration</th>
                     <td>3,513</td>
                     <td>294</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      36,49%
-                    </td>
+                    <td>10</td>
                   </tr>
                   <tr>
-                    <th scope="row">/argon/tables.html</th>
+                    <th scope="row">Logistics</th>
                     <td>2,050</td>
                     <td>147</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/profile.html</th>
-                    <td>1,795</td>
-                    <td>190</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-danger mr-3" />{" "}
-                      46,53%
-                    </td>
+                    <td>20</td>
                   </tr>
                 </tbody>
               </Table>
@@ -263,10 +246,6 @@ const Index = (props) => {
                   <tr>
                     <th scope="row">Germany</th>
                     <td>4</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Finland</th>
-                    <td>1</td>
                   </tr>
                 </tbody>
               </Table>
